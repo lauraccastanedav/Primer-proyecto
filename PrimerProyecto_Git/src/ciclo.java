2a54fa33da3f04e63887b00034c7a1cd76c0278f
Laura Cristina Castañeda <22517175@live.uem.es>
2025-11-06 18:54:22 +0100

    Primer commit
    Primer commit con la clase ciclo.java.

---
+++ PrimerProyecto_Git/src/ciclo.java
@@ -0,0 +1,9 @@
+
+public class ciclo {
+
+	public static void main(String[] args) {
+		System.out.println("Técnico Superior en Desarrollo de Aplicaciones");
+
+	}
+
+}
---
+++ PrimerProyecto_Git/src/nombre.java
@@ -0,0 +1,9 @@
+
+public class nombre {
+
+	public static void main(String[] args) {
+		System.out.println("Laura Cristina Castañeda Vasquez");
+
+	}
+
+}
