89097e489ea580f385d7bc41e97edec75b55e576
Laura Cristina Castañeda Vasquez <lauraccastanedav@gmail.com>
2025-11-24 20:57:03 +0100

    Revert "Eliminación texto Gitkraken"
    This reverts commit f84a7c3d5e392f4189528fd19841fb87c164a2df.

--- PrimerProyecto_Git/src/nombre.java
+++ PrimerProyecto_Git/src/nombre.java
@@ -4,7 +4,6 @@ public class nombre {
 	public static void main(String[] args) {
 		System.out.println("Laura Cristina, 666666");
 		System.out.println("Esto es el tercer error");
-		System.out.println("Otra prueba");
 	}
 
 }
